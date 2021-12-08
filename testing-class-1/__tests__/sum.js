56e87cee0bdf73c2d0fc65793d89f0b0150d2b33
Deepak Chaurasiya <dchaurasiya8589@gmail.com>
2021-12-08 09:28:36 +0530

    unit testing

---
+++ testing-class-1/__tests__/sum.js
@@ -0,0 +1,9 @@
+const sum = require("../index")
+
+test("Addition of two positive umbers",function(){
+    expect(1+3).toBe(4);
+})
+
+test("substraction of two  numbers",function(){
+    expect(-4 -66).toBe(-70);
+})
---
+++ testing-class-1/index.js
@@ -0,0 +1,7 @@
+const sum = (a,b)=>{
+    return a+b
+}
+
+console.log(sum(5,4))
+
+module.exports = sum
